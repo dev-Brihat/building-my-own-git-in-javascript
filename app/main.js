b2efcbf0ba56c12a64ed18c3cf8038db86a7f082
dev-Brihat <contact.brihat@gmail.com>
2024-06-26 15:04:51 +0545

    Added the cat-file implementation to read blob object using zlib decompression

--- app/main.js
+++ app/main.js
@@ -1,12 +1,18 @@
 const fs = require("fs");
 const path = require("path");
+const { deflateRawSync } = require("zlib");
 
 
 const command = process.argv[2];
+const flag = process.argv[3];
 
 switch (command) {
   case "init":
     createGitDirectory();
+    break;
+  case "cat-file":
+    const hash_code = process.argv[4];
+    catFile(hash_code);
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -19,4 +25,16 @@ function createGitDirectory() {
 
   fs.writeFileSync(path.join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
   console.log("Initialized git directory");
+}
+
+function catFile(hash_code){
+  if (flag === '-p'){
+    const content = fs.readFileSync(path.join(process.cwd(), ".git", "objects", hash_code.slice(0, 2), hash_code.slice(2)));
+    const decompressed_data = zlib.deflateRawSync(content);
+    const result = decompressed_data.toString().split("\x00")[1];
+
+    process.stdout.write(result);
+  } else {
+    throw new Error(`Invalid Flag ${flag}`)
+  }
 }
